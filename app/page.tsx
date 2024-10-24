c65851f03af1c82a1bf679afe92ea40ad93f3943
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-10-23 20:56:29 -0700

    fix wrap issue with useSearchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import MainView from "./components/main_view";
-
+import { Suspense } from "react";
 export default function Home() {
   return (
-    <MainView />
+    <Suspense fallback={<div>Loading...</div>}>
+      <MainView />
+    </Suspense>
   )
 }
